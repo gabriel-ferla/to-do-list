f87db089619888ed2f4eff3ba0bb0f9b6e47b163
gabriel-ferla <123134347+gabriel-ferla@users.noreply.github.com>
2023-03-14 22:33:33 -0300

    funções em botões, formulário edição

--- js/script.js
+++ js/script.js
@@ -6,6 +6,8 @@ const editForm = document.querySelector("#edit-form")
 const editInput = document.querySelector("#edit-input")
 const cancelEditBtn = document.querySelector("#cancel-edit-btn")
 
+let oldInputValue
+
 
 // Funções
 const saveTodo = (text) => {
@@ -37,6 +39,27 @@ const saveTodo = (text) => {
     todoInput.focus()
 }
 
+//Esconde formulário e abre pg de edição
+const toggleForms = () => {
+    editForm.classList.toggle("hide")
+    todoForm.classList.toggle("hide")
+    todoList.classList.toggle("hide")
+}
+
+//Atualizar titulo lista
+const updateTodo = (text) => {
+    const todos = document.querySelectorAll(".todo")
+
+    todos.forEach((todo) => {
+        
+        let todoTitle = todo.querySelector("h3")
+
+        if(todoTitle.innerText === oldInputValue) {
+            todoTitle.innerText = text
+        }
+    })
+}
+
 // Eventos
 todoForm.addEventListener("submit", (e) => {
     e.preventDefault()
@@ -47,4 +70,53 @@ todoForm.addEventListener("submit", (e) => {
     if(inputValue) {
         saveTodo(inputValue)
     }
+})
+
+document.addEventListener("click", (e) => {
+    const targetEl = e.target
+    const parentEl = targetEl.closest("div") //seleciona a div mais próxima do parent = (Botão clicado), no caso (todo done)
+    let todoTitle
+
+    if(parentEl && parentEl.querySelector("h3")) {
+        todoTitle = parentEl.querySelector("h3").innerText
+    }
+
+    //definir a tarefa como completa
+    if(targetEl.classList.contains("finish-todo")) {
+        parentEl.classList.toggle("done")
+    }
+
+
+    //remover tarefa
+    if(targetEl.classList.contains("remove-todo")) {
+        parentEl.remove()
+    }
+
+    //editar tarefa
+    if(targetEl.classList.contains("edit-todo")) {
+        toggleForms()
+
+        editInput.value = todoTitle
+        oldInputValue = todoTitle
+    }
+})
+
+//Cancelar edição de tarefa, voltar pg inicial
+cancelEditBtn.addEventListener("click", (e) => {
+    e.preventDefault()
+
+    toggleForms()
+})
+
+//Confirmar edição
+editForm.addEventListener("submit", (e) => {
+    e.preventDefault()
+
+    const editInputValue = editInput.value
+
+    if(editInputValue) {
+        updateTodo(editInputValue)
+    }
+
+    toggleForms()
 })
